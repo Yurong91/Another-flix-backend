45014d6a6746541b7ec7af6765ba0f05216acd69
Yurong91 <yuronghou@outlook.com>
2022-06-17 09:08:58 -0500

    authorization and authentication working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const usersCtrl = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 // POST /api/v1/users
 router.post('/', usersCtrl.create)
 // POST /api/v1/users/login
-// router.post('/login', usersCtrl.login)
-// GET /api/v1/users/:id
-router.get('/:id', usersCtrl.show)
-// PUT /api/v1/users/:id
-router.put('/:id', usersCtrl.update)
+router.post('/login', usersCtrl.login)
 
-router.get('/:id/favorites',usersCtrl.getFavorites )
+// The below routes should not be accessible to unauthorized users
+
+// GET /api/v1/users/:id
+router.get('/:id', ensureLoggedIn, usersCtrl.show)
+// GET /api/v1/users/:id/favorites
+router.get('/:id/favorites', ensureLoggedIn, usersCtrl.getFavorites)
+// PUT /api/v1/users/:id
+router.put('/:id', ensureLoggedIn, usersCtrl.update)
 
 module.exports = router
